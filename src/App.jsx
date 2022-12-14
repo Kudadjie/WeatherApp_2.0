1198e95da51c23c13cef497dc98dd15b35f16c34
John David K.T. Kudadjie <johnkudadjiekabutetteh@outlook.com>
2022-12-14 18:26:25 +0000

    Updated Footer component and associated styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
+import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
