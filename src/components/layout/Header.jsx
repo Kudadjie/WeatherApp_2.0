4ce50a663ba85a74bba0c3c5bdaa7401650236a8
John David K.T. Kudadjie <johnkudadjiekabutetteh@outlook.com>
2022-12-14 20:32:20 +0000

    Updated Header component, index style file
    - Updated for mobile view

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,7 +2,7 @@ import logo from "../../assets/images/weatherNowLogo.svg";
 export default function Header() {
   return (
     <header>
-      <img src={logo} alt="Weather Now Logo" />
+      <img src={logo} style={{ margin: "0 0 0 30px" }} alt="Weather Now Logo" />
     </header>
   );
 }
