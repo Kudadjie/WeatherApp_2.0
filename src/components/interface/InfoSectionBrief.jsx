7ac92b8bbe49a4c49f9ba23b802c35c671b4e9d7
John David K.T. Kudadjie <johnkudadjiekabutetteh@outlook.com>
2022-12-15 15:20:11 +0000

    Updated InfoSectionBrief sub component
    - Built UI, pending state management

--- src/components/interface/InfoSectionBrief.jsx
+++ src/components/interface/InfoSectionBrief.jsx
@@ -1,9 +1,43 @@
+import locationPin from "../../assets/images/icons/location.svg";
+import dayIcon from "../../assets/images/icons/dayNight/sun.png";
+
+// import nightIcon from "../../assets/images/icons/dayNight/moon.png";
+
 import "./scss/InfoSectionBrief.scss";
 export default function InfoSectionBrief() {
   return (
-    <div className="panel panel__infoSectionBrief">
-      <div>Location Details</div>
-      <div>Time of Day Icon</div>
+    <div className="infoSectionBrief">
+      <div>
+        <h4 className="infoSectionBrief__location">location</h4>
+        <button className="infoSectionBrief__lastUpdated">
+          {/* //render with time */}
+          last updated just now
+          <i className="fa-solid fa-arrow-rotate-right"></i>
+        </button>
+        <br />
+        <button className="infoSectionBrief__changeLocation">
+          <img
+            src={locationPin}
+            width="25"
+            style={{ marginRight: "8px" }}
+            alt="#"
+          />
+          change location
+          {/* <span className="span-block change-location">
+            
+          </span> */}
+        </button>
+      </div>
+
+      <img
+        src={dayIcon}
+        alt="#"
+        width="30"
+        height="30"
+        style={{ marginTop: "10px" }}
+      />
+      {/* Conditionally render */}
+      {/* <img src={nightIcon} alt="#" width="30" height="30" /> */}
     </div>
   );
 }
