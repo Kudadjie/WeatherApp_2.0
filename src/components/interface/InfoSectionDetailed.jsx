8073e71d093a83cc1d224f1f247f99b27d13d366
John David K.T. Kudadjie <johnkudadjiekabutetteh@outlook.com>
2022-12-15 18:48:13 +0000

    Updated infoSectionDetailed sub component
    - Built UI, Changed Panel component background for clarity

--- src/components/interface/InfoSectionDetailed.jsx
+++ src/components/interface/InfoSectionDetailed.jsx
@@ -1,7 +1,40 @@
 import "./scss/InfoSectionDetailed.scss";
-
-//import all images
+import windIcon from "../../assets/images/wind.svg";
+import humidityIcon from "../../assets/images/humidity.svg";
+//import all images and choose based on state.
 
 export default function InfoSectionDetailed() {
-  return <></>;
+  return (
+    <div className="infoSectionDetailed">
+      {/*render actual heading */}
+      <h2 className="infoSectionDetailed__heading">Heading</h2>
+      <div className="infoSectionDetailed__details">
+        <div>
+          {/*render details */}
+          <p>Chance of rain :</p>
+          <p>Sunrise | Sunset : </p>
+          <p> Moonrise | Moonset : </p>
+        </div>
+        <div>
+          {/* render details*/}
+          <p className="text-spacing values-section">
+            <i className="fa-solid fa-droplet icons-breathe"></i> Precipitation:{" "}
+            {"--"}
+          </p>
+          <p className="text-spacing values-section">
+            <span>
+              <img alt="#" src={windIcon} width="25" />
+            </span>{" "}
+            Wind: {"--"}
+          </p>
+          <p className="text-spacing values-section">
+            <span>
+              <img alt="#" src={humidityIcon} width="25" />
+            </span>{" "}
+            Humidity: {"--"}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
