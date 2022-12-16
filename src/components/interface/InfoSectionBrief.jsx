f595bc1a2b56a23a0416cac54ad43dd5deb8f1e1
John David K.T. Kudadjie <johnkudadjiekabutetteh@outlook.com>
2022-12-16 17:00:33 +0000

    Cleaned house and adjusted for views

--- src/components/interface/InfoSectionBrief.jsx
+++ src/components/interface/InfoSectionBrief.jsx
@@ -1,13 +1,19 @@
 import locationPin from "../../assets/images/icons/location.svg";
 import dayIcon from "../../assets/images/icons/dayNight/sun.png";
-// import weatherSVG from "../../assets/images/icons/weather/64x64/day/143.png";
 
 // import nightIcon from "../../assets/images/icons/dayNight/moon.png";
 
 import "./scss/InfoSectionBrief.scss";
 export default function InfoSectionBrief() {
-  //Based on state
-  // const renderWeatherImage = <img src={weatherSVG} alt="#" width="60%" />
+  const Icon = (
+    <img
+      src={dayIcon}
+      alt="#"
+      width="30"
+      height="30"
+      style={{ marginTop: "10px" }}
+    />
+  );
   return (
     <section>
       <div className="infoSectionBriefUpper">
@@ -32,21 +38,12 @@ export default function InfoSectionBrief() {
           </span> */}
           </button>
         </div>
-
-        <img
-          src={dayIcon}
-          alt="#"
-          width="30"
-          height="30"
-          style={{ marginTop: "10px" }}
-        />
+        {Icon}
         {/* Conditionally render */}
         {/* <img src={nightIcon} alt="#" width="40" height="40" /> */}
       </div>
       {/* Only for mobile */}
       <div className="infoSectionBriefLower">
-        {/* render only in non mobile views and dynamically chosen*/}
-        {/* renderWeatherImage */}
         <div className="infoSectionBriefLower__temperature">
           <h1>31</h1>
           <div className="infoSectionBriefLower__temperatureUnit">
