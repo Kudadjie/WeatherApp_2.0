9523e77586c11c9cd78dd9fb2af79c1707a9a117
John David K.T. Kudadjie <johnkudadjiekabutetteh@outlook.com>
2022-12-19 19:37:06 +0000

    Updated InfoSectionDetailed sub component
    - Dynamically display content

--- src/components/interface/InfoSectionDetailed.jsx
+++ src/components/interface/InfoSectionDetailed.jsx
@@ -1,49 +1,55 @@
 import "./scss/InfoSectionDetailed.scss";
 import windIcon from "../../assets/images/wind.svg";
 import humidityIcon from "../../assets/images/humidity.svg";
-//import all images and choose based on state.
+import { useSelector } from "react-redux";
 
 export default function InfoSectionDetailed() {
-  const [chanceOfRain, sunRiseSet, moonRiseSet, precipitation, wind, humidity] =
-    [0, 0, 0, 0, 0, 0];
+  const ctx = useSelector((state) => {
+    return {
+      precipitation: `${state.currentWeather.precipitation} in`,
+      wind: `${state.currentWeather.wind} mph`,
+      humidity: `${state.currentWeather.humidity} %`,
+      chanceOfRain: `${state.currentWeather.chanceOfRain} %`,
+      moonRiseSet: state.riseSetValues.moon,
+      sunRiseSet: state.riseSetValues.sun,
+    };
+  });
+
   return (
     <div className="infoSectionDetailed">
-      {/*render actual heading */}
       <h2 className="infoSectionDetailed__heading">Heading</h2>
       <div className="infoSectionDetailed__details">
         <div>
-          {/*render details */}
           <p>
-            Chance of rain: <br /> {chanceOfRain}
+            Chance of rain: <br /> {ctx.chanceOfRain}
           </p>
           <p>
-            Sunrise | Sunset: <br /> {sunRiseSet}
+            Sunrise | Sunset: <br /> {ctx.sunRiseSet}
           </p>
           <p>
             {" "}
-            Moonrise | Moonset: <br /> {moonRiseSet}
+            Moonrise | Moonset: <br /> {ctx.moonRiseSet}
           </p>
         </div>
         <div>
-          {/* render details*/}
-          <p className="text-spacing values-section">
+          <p>
             <i className="fa-solid fa-droplet icons-breathe"></i> Precipitation:{" "}
             <br />
-            {precipitation}
+            {ctx.precipitation}
           </p>
-          <p className="text-spacing values-section">
+          <p>
             <span>
               <img alt="#" src={windIcon} width="25" />
             </span>{" "}
             Wind: <br />
-            {wind}
+            {ctx.wind}
           </p>
-          <p className="text-spacing values-section">
+          <p>
             <span>
               <img alt="#" src={humidityIcon} width="25" />
             </span>{" "}
             Humidity: <br />
-            {humidity}
+            {ctx.humidity}
           </p>
         </div>
       </div>
