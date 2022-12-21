25021c71a2b30961d3681d320883813dfa063a4d
John David K.T. Kudadjie <johnkudadjiekabutetteh@outlook.com>
2022-12-21 12:27:35 +0000

    Updated ChangeLocationModal component
    Completed UI for responsive design

--- src/components/interface/ChangeLocationModal.jsx
+++ src/components/interface/ChangeLocationModal.jsx
@@ -1,9 +1,25 @@
 import "./scss/ChangeLocationModal.scss";
 export default function ChangeLocationModal() {
+  function closeModalHandler(event) {
+    event.preventDefault();
+  }
+
+  function searchHandler(event) {
+    event.preventDefault();
+  }
   return (
     <div className="modal">
-      <form className="changeLocationForm">
-        <button className="changeLocationForm__close-btn">X close</button>
+      <form className="changeLocationForm" onSubmit={searchHandler}>
+        <div>
+          <button
+            className="changeLocationForm__close-btn"
+            style={{ width: "fit-content" }}
+            onClick={closeModalHandler}
+          >
+            X close
+          </button>
+        </div>
+
         <input type="text" />
         <button type="submit">Search</button>
       </form>
